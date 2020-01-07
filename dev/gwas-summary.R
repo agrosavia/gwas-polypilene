089cc330538194c380c2c53b4e4197fb36a70be3
Luis Garreta - Agrosavia <lgarreta@agrosavia.co>
2020-01-07 18:44:14 -0500

    r5.0: Running rtassel GLM and MLM

--- dev/gwas-summary.R
+++ dev/gwas-summary.R
@@ -12,19 +12,18 @@ options (width=300)
 #------------------------------------------------------------------------
 # Create Venn diagram of common markers using info from summary table
 #------------------------------------------------------------------------
-markersVennDiagrams <- function (summaryTable, typeScores){
+markersVennDiagrams <- function (summaryTable, scoresType, outDir="out"){
 	require(VennDiagram)
 	flog.threshold(ERROR)
 	x <- list()
 	x$Gwasp4 = summaryTable %>% filter (TOOL %in% "Gwasp4") %>% select (SNP) %>% .$SNP
-	x$Gwasp2 = summaryTable %>% filter (TOOL %in% "Gwasp2") %>% select (SNP) %>% .$SNP
+	x$Shesis = summaryTable %>% filter (TOOL %in% "Shesis") %>% select (SNP) %>% .$SNP
 	x$Plink  = summaryTable %>% filter (TOOL %in% "Plink")  %>% select (SNP) %>% .$SNP
 	x$Tassel = summaryTable %>% filter (TOOL %in% "Tassel") %>% select (SNP) %>% .$SNP
 
-	v0 <<-venn.diagram(x, height=9000, width=9000,
-										col = c("red", "blue", "green", "yellow"),
-										fill = c("red", "blue", "green", "yellow"), 
-										alpha = 0.5, filename = NULL)
+	v0 <-venn.diagram(x, height=9000, width=9000, alpha = 0.5, filename = NULL,
+						col = c("red", "blue", "green", "yellow"),
+						fill = c("red", "blue", "green", "yellow")) 
 
 	overlaps <- calculate.overlap(x)
 	overlaps <- rev(overlaps)
@@ -33,35 +32,35 @@ markersVennDiagrams <- function (summaryTable, typeScores){
 	for (i in 1:length(overlaps)){
 		pos = posOverlap [i]
 		v0[[pos+8]]$label <- paste(overlaps[[i]], collapse = "\n")
-	}
-
-	pdf(paste0("out-venn-diagram-", typeScores, ".pdf"))
+ 	}
+ 
+	pdf(paste0(outDir,"/out-summary-gwas-venndiagram-", scoresType, ".pdf"))
 	grid.draw(v0)
 	dev.off()
 }
 #------------------------------------------------------------------------
 # Create a summary table of best and significative markers
 #------------------------------------------------------------------------
-markersSummaryTable <- function (inputDir, gwasType, nBEST=10, SIGNIFICANCE=0.05) {
+markersSummaryTable <- function (inputDir, gwasType, outDir="out", nBEST=5, SIGNIFICANCE=0.05) {
 	nMARKERS = 2017
 	THRESHOLD = round (-log10 (SIGNIFICANCE/nMARKERS),4)
 
 	files =  list.files(inputDir, pattern=paste0("^(.*(",gwasType,").*(scores)[^$]*)$"), full.names=T)
-	print (files)
-	#files = c("out-Gwasp4-Naive-significativeQTLs.tbl", "out-Gwasp2-Naive-significativeQTLs.tbl", 
-	#		  "out-Plink-Naive-assoc.linear.adjusted.tbl", "out-Tassel-Naive-GLM_Stats_geno+pheno.tbl")
-	#files = c("out-Gwasp4-Naive-significativeQTLs.tbl", 
+	message (">>>> SUMMARY FOR THE FILES: ")
+	for (f in files) message ("    ", f)
 	summaryTable = data.frame ()
 
+	message ("");message (">>>> Processing files")
 	tool=""
 	for (f in files) {
-		data = read.table (file=f, header=T)
-		if (str_detect(f, "Gwasp")) {
-			if (str_detect(f, "Gwasp4")) tool <- "Gwasp4" else tool <- "Gwasp2"
+		message ("    ", f)
+		if (str_detect(f, "Gwasp4")) {
+			#if (str_detect(f, "Gwasp4")) tool <- "Gwasp4" else tool <- "Gwasp2"
+			data = read.table (file=f, header=T)
+			tool    = "Gwasp4"
 			data    = data %>% add_count (Marker, sort=T, name="N1") %>% arrange (desc(N1), desc(Score))
 			data    = data %>% distinct (Marker, .keep_all=T)
-			#write.table (file=paste0("data.scores-",tool,".scores"), data, quote=F, sep="\t")
-			data    = data [1:nBEST,]
+			data    = if (ncol(data)>nBEST) data [1:nBEST,] else data
 			snps    <- data$Marker
 			pVal	<- round (10^(-data$Score),10)
 			pscores <- data$Score
@@ -70,7 +69,8 @@ markersSummaryTable <- function (inputDir, gwasType, nBEST=10, SIGNIFICANCE=0.05
 			pos	    <- data$Position
 			signf   = pscores >= tscores
 		}else if (str_detect (f, "Plink")) {
-			data    = data [1:nBEST,]
+			data = read.table (file=f, header=T)
+			data    = if (ncol(data)>nBEST) data [1:nBEST,] else data
 			tool    = "Plink"
 			snps    = data$SNP
 			pVal    = p.adjust (data$UNADJ, "fdr")
@@ -81,18 +81,21 @@ markersSummaryTable <- function (inputDir, gwasType, nBEST=10, SIGNIFICANCE=0.05
 			signf   = pscores >= tscores
 			
 		}else if (str_detect (f, "Tassel")) {
-			data    = data %>% top_n (-1*nBEST,p)
+			data    = read.table (file=f, header=T)
+			data    = if (ncol(data)>nBEST) data [1:nBEST,] else data
 			tool    = "Tassel"
 			snps    = data$Marker
-			pVal    = unlist (data %>% rowwise %>% mutate (minP=min(p, add_p, dom_p, na.rm=T)) %>% select (minP))
-			pVal    = p.adjust (pVal, "fdr")
+			#pVal    = unlist (data %>% rowwise %>% mutate (minP=min(p, add_p, dom_p, na.rm=T)) %>% select (minP))
+			#pVal    = p.adjust (pVal, "fdr")
+			pVal    = data$minP
 			pscores = unlist (round (-log10 (pVal),4))
 			tscores = THRESHOLD
 			chrom   = data$Chr
 			pos		= data$Pos
 			signf   = pscores >= tscores
 		}else if (str_detect (f, "Shesis")) {
-			data    = data %>% top_n (-1*nBEST,P.value)
+			data    = read.table (file=f, header=T, sep="\t")
+			data    = data %>% arrange (P.value) %>% head (nBEST) #top_n (-1*nBEST,P.value)
 			tool    = "Shesis"
 			snps    = data$SNP
 			pVal    = data$P.value
@@ -103,25 +106,23 @@ markersSummaryTable <- function (inputDir, gwasType, nBEST=10, SIGNIFICANCE=0.05
 			pos		= NA
 			signf   = pscores >= tscores
 		}
-	
-		#data    = data %>% top_n (-1*nBEST,pscores)
 		print (tool)
-		print (length (pVal))
-		dfm <- data.frame (TOOL=tool, MODEL=gwasType, CHR=chrom, POS=pos, SNP=snps, P = round (pVal,6), FDRSCORE=pscores, THRESHOLD=tscores, SIGNF=signf )
+		dfm = data.frame (TOOL=tool, MODEL=gwasType, CHR=chrom, POS=pos, SNP=snps, P = round (pVal,6), FDRSCORE=pscores, THRESHOLD=tscores, SIGNF=signf )
 		dfm = dfm %>% distinct (SNP, .keep_all=T)
 		summaryTable <- rbind (summaryTable, dfm)
 	}
 	summaryTable = summaryTable [which(!is.na(summaryTable$SIGNF)),]
 	message ("Writing...")
-	outName = paste0("out-summary-gwas-best", nBEST)
+	outName = paste0(outDir, "/out-summary-gwas-best", nBEST)
 	write.table (file=paste0(outName,".scores"), summaryTable, row.names=F,quote=F, sep="\t")
-	markersVennDiagrams (summaryTable, paste0("best",nBEST))
+	markersVennDiagrams (summaryTable, paste0("best",nBEST), outDir)
 	summarySignificatives = summaryTable %>% filter (SIGNF%in%T) 
-	write.table (file="out-summary-gwas-significatives.scores", summarySignificatives, row.names=F,quote=F, sep="\t")
-	markersVennDiagrams (summarySignificatives, "significatives")
+	outName = paste0(outDir, "/out-summary-gwas-signficatives.scores")
+	write.table (file=outName, summarySignificatives, row.names=F,quote=F, sep="\t")
+	markersVennDiagrams (summarySignificatives, "significatives", outDir)
 
 	return (summaryTable)
 }
 
 # Create Venn diagram of common markers
-markersSummaryTable ("out/", "Naive")
+#markersSummaryTable ("out/", "Naive", "out/")
--- gwas-polypilene-profile.sh
+++ gwas-polypilene-profile.sh
@@ -1,3 +1,8 @@
+AGRO_HOME=/home/lg/agrosavia
 GWAS_BIN=/home/lg/agrosavia/bin
 export PATH=$PATH:$GWAS_BIN
 
+# Tassel pipeline home
+export TASSEL_HOME=$AGRO_HOME/opt/TASSEL5
+
+export PATH=$PATH:$TASSEL_HOME/
