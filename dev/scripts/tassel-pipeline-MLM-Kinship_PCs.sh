59443b97be1b32f23cefacfafd90539c68cdeb95
Luis Garreta - Agrosavia <lgarreta@agrosavia.co>
2020-01-09 14:32:54 -0500

    r5.5: Commands as bashs scripts, log files for stdout and errors

--- dev/scripts/tassel-pipeline-MLM-Kinship_PCs.sh
+++ dev/scripts/tassel-pipeline-MLM-Kinship_PCs.sh
@@ -9,13 +9,32 @@ GENOMAP=$2
 PHENOTBL=$3
 OUTFILE=$4
 
+##$TASSEL_HOME/run_pipeline.pl \
+#	-fork1 -plink -ped $GENOPED -map $GENOMAP -PrincipalComponentsPlugin -covariance -reportEigenvalues false -reportEigenvectors false -endPlugin \
+#	-fork2 -r $PHENOTBL \
+#	-combine3 -input1 -input2 -intersect \
+#	-fork4 -plink -ped $GENOPED -map $GENOMAP -KinshipPlugin -method Dominance_Centered_IBS -endPlugin \
+#	-combine5 -input3 -input4 \
+#	-mlm -mlmVarCompEst P3D \
+#	-mlmOutputFile $OUTFILE
+
 $TASSEL_HOME/run_pipeline.pl \
 	-fork1 -plink -ped $GENOPED -map $GENOMAP \
 	-fork2 -r $PHENOTBL \
-	-fork4 -plink -ped $GENOPED -map $GENOMAP -PrincipalComponentsPlugin -covariance -reportEigenvalues false -reportEigenvectors false -endPlugin \
-    -combine3 -input1 -input2 -input4 -intersect \
+	-fork3 -plink -ped $GENOPED -map $GENOMAP -PrincipalComponentsPlugin -covariance -reportEigenvalues false -reportEigenvectors false -endPlugin \
+    -combine4 -input1 -input2 -input3 -intersect \
 	-fork5 -plink -ped $GENOPED -map $GENOMAP -KinshipPlugin -method Dominance_Centered_IBS -endPlugin \
-	-combine6 -input3 -input5 \
+    -combine6 -input4 -input5 \
 	-mlm -mlmVarCompEst P3D \
 	-mlmOutputFile $OUTFILE
+
+#$TASSEL_HOME/run_pipeline.pl \
+#	-fork1 -plink -ped $GENOPED -map $GENOMAP \
+#	-fork2 -r $PHENOTBL \
+#	-fork4 -plink -ped $GENOPED -map $GENOMAP -PrincipalComponentsPlugin -covariance -reportEigenvalues false -reportEigenvectors false -endPlugin \
+#   -combine3 -input1 -input2 -input4 -intersect \
+#	-fork5 -plink -ped $GENOPED -map $GENOMAP -KinshipPlugin -method Dominance_Centered_IBS -endPlugin \
+#	-combine6 -input3 -input5 \
+#	-mlm -mlmVarCompEst P3D \
+#	-mlmOutputFile $OUTFILE
 
